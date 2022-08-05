3c52c05cb2fc026bf3e2b520cd8251a7c1cbda87
shourovahmd <moh.abuhanifa@gmail.com>
2022-08-06 04:59:24 +0600

    labels updated

--- src/components/Labels.js
+++ src/components/Labels.js
@@ -1,39 +1,41 @@
 import React from "react";
 const obj = [
-    {
-        type: "Savings",
-        color: '#f9c74f',
-        percent: 45
-    },
-    {
-        type: "Investment",
-        color: '#f9c74f',
-        percent: 20
-    },
-    {
-        type: "Expense",
-        color:  'rgb(54, 162, 235)',
-        percent: 10
-    }
-]
-function LabelComponent() {
+  {
+    type: "Savings",
+    color: "#f9c74f",
+    percent: 45,
+  },
+  {
+    type: "Investment",
+    color: "#f9c74f",
+    percent: 20,
+  },
+  {
+    type: "Expense",
+    color: "rgb(54, 162, 235)",
+    percent: 10,
+  },
+];
+function LabelComponent({ data }) {
   return (
     <div className="labels flex justify-between">
       <div className="flex gap-2">
         <div
           className="w-2 h-2 rounded py-3"
-          style={{ background: "#f9c74f" }}
+          style={{ background: data.color ?? "#f9c74f" }}
         ></div>
         <h3 className="text-md">Savings</h3>
       </div>
-      <h3 className="font-bold">45%</h3>
+      <h3 className="font-bold">{data.percent ?? 0}%</h3>
     </div>
   );
 }
 const Labels = () => {
   return (
     <>
-      {obj.map((v, i) => <LabelComponent key={i} data={v}></LabelComponent>)}
+      {obj.map((v, i) => (
+        <LabelComponent key={i} data={v}></LabelComponent>
+      ))}
     </>
   );
 };
