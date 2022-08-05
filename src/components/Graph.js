d5319fa644bc37d921b6ab6a934769bcdaa99900
shourovahmd <moh.abuhanifa@gmail.com>
2022-08-06 04:22:23 +0600

    doughnut added

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,11 +1,40 @@
-import React from 'react';
-
+import { ArcElement, Chart } from 'chart.js';
+import React from "react";
+import { Doughnut } from 'react-chartjs-2';
+Chart.register(ArcElement);
 const Graph = () => {
-    return (
-        <div className="flex justify-content max-w-max mx-auto" >
-            graph
+    const data = {
+        labels: [
+          'Red',
+          'Blue',
+          'Yellow'
+        ],
+        datasets: [{
+          label: 'My First Dataset',
+          data: [300, 50, 100],
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+          ],
+          hoverOffset: 4
+        }]
+      };
+  return (
+    <div className="flex justify-content max-w-max mx-auto">
+      <div className="item">
+        <div className="chart relative">
+          <h3 className="mb-4 font-bold title">
+            Total
+            <Doughnut data={data}/>
+            <span className="block text-3xl text-emerald-400"></span>
+          </h3>
         </div>
-    );
+
+        <div className="flex flex-col py-10 gap-4"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Graph;
