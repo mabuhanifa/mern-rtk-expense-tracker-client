0c7acc02d0127fea1f9a19041c72d54050c00b2d
shourovahmd <moh.abuhanifa@gmail.com>
2022-08-06 04:00:49 +0600

    app updated

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <div>
-        <h1>Expense Tracker</h1>
+    <div className="app">
+      <div className="container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800">
+        <h1 className="text-4xl py-8 mb-10 bg-slate-800 text-white rounded-xl">
+          Expense Tracker
+        </h1>
+        <div className="grid md:grid-cols-2 gap-4">
+          { /* chart */ }
+        </div>
       </div>
     </div>
   );
