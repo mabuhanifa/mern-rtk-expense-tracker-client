905cb0d8647f62044f7d3589470baeab8c0d9abd
shourovahmd <moh.abuhanifa@gmail.com>
2022-08-06 04:27:35 +0600

    doughnut updated

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -3,31 +3,39 @@ import React from "react";
 import { Doughnut } from 'react-chartjs-2';
 Chart.register(ArcElement);
 const Graph = () => {
-    const data = {
-        labels: [
-          'Red',
-          'Blue',
-          'Yellow'
-        ],
-        datasets: [{
-          label: 'My First Dataset',
-          data: [300, 50, 100],
-          backgroundColor: [
-            'rgb(255, 99, 132)',
-            'rgb(54, 162, 235)',
-            'rgb(255, 205, 86)'
-          ],
-          hoverOffset: 4
-        }]
-      };
+    const config ={
+        data:{
+            labels: [
+              'Red',
+              'Blue',
+              'Yellow'
+            ],
+            datasets: [{
+              label: 'My First Dataset',
+              data: [300, 50, 100],
+              backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 205, 86)'
+              ],
+              hoverOffset: 4,
+              borderRadius:30,
+              spacing:10,
+            }]
+          },
+          options:{
+            cutout:115
+          }
+    } 
   return (
     <div className="flex justify-content max-w-max mx-auto">
       <div className="item">
         <div className="chart relative">
+            <Doughnut {...config}/>
           <h3 className="mb-4 font-bold title">
-            Total
-            <Doughnut data={data}/>
-            <span className="block text-3xl text-emerald-400"></span>
+            <span className="block text-3xl text-emerald-400">
+                ${0}
+            </span>
           </h3>
         </div>
 
