ad4730c217730872f3589b46e41addb278ce8992
shourovahmd <moh.abuhanifa@gmail.com>
2022-08-06 06:21:46 +0600

    list added

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,22 +1,25 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const Forms = () => {
+  const { register, handleSubmit, resetField } = useForm();
   const onSubmit = (data) => {
     console.log(data);
   };
   return (
     <div className="form max-w-sm mx-auto w-96">
       <h1 className="font-bold pb-4 text-xl">Transaction</h1>
-      <form id="form" onSubmit={onSubmit}>
+      <form id="form" onSubmit={handleSubmit(onSubmit)}>
         <div className="grid gap-4">
           <div className="input-group">
             <input
               type="text"
-              placeholder="Salary, House Rend, SIP"
-              className={"form_input"}
+              {...register("name")}
+              placeholder="Sallary, House Rend, SIP"
+              className="form-input"
             />
           </div>
-          <select className={"form_input"}>
+          <select className="form-input">
             <option value="Investment" defaultValue>
               Investment
             </option>
@@ -24,7 +27,12 @@ const Forms = () => {
             <option value="Savings">Savings</option>
           </select>
           <div className="input-group">
-            <input type="text" placeholder="Amount" className={"form_input"} />
+            <input
+              type="text"
+              {...register("amount")}
+              placeholder="Amount"
+              className="form-input"
+            />
           </div>
           <div className="submit-btn">
             <button className="border py-2 text-white bg-indigo-500 w-full">
---
+++ src/components/List.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const List = () => {
+  return (
+    <div>
+      <div>list</div>
+    </div>
+  );
+};
+
+export default List;
