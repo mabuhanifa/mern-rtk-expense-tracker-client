f1ae557855906def687ede49976f550c314c3d86
shourovahmd <moh.abuhanifa@gmail.com>
2022-08-06 06:11:40 +0600

    fixed @apply issue

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -4,7 +4,6 @@ const Forms = () => {
   const onSubmit = (data) => {
     console.log(data);
   };
-  const form_input = `mt-1 block w-full py-2 px-3 border border-gray-200 bg-white rounded-md focus:outline-none focus:ring-indigo-500 sm:text-sm`;
   return (
     <div className="form max-w-sm mx-auto w-96">
       <h1 className="font-bold pb-4 text-xl">Transaction</h1>
@@ -14,10 +13,10 @@ const Forms = () => {
             <input
               type="text"
               placeholder="Salary, House Rend, SIP"
-              className={form_input}
+              className={"form_input"}
             />
           </div>
-          <select className={form_input}>
+          <select className={"form_input"}>
             <option value="Investment" defaultValue>
               Investment
             </option>
@@ -25,7 +24,7 @@ const Forms = () => {
             <option value="Savings">Savings</option>
           </select>
           <div className="input-group">
-            <input type="text" placeholder="Amount" className={form_input} />
+            <input type="text" placeholder="Amount" className={"form_input"} />
           </div>
           <div className="submit-btn">
             <button className="border py-2 text-white bg-indigo-500 w-full">
